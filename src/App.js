a72dfb9a00cde4754b9f4265979af32bb7de170c
alice-ibba5 <135819129+alice-ibba5@users.noreply.github.com>
2023-12-22 17:21:33 +0100

    up

--- src/App.js
+++ src/App.js
@@ -3,23 +3,22 @@ import "./App.scss";
 import Home from "./components/Home";
 import Main from "./components/Main";
 import SpecificProfileViewer from "./components/SpecificProfileViewer";
-import home from "./components/home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-    <home>
-      <Routes>
-        {<Route path="/" element={<Home />}></Route>}
-        {<Route path="/:id" element={<Main />} />}
-        {
-          <Route
-            path="/:SpecificProfile/:id"
-            element={<SpecificProfileViewer />}
-          />
-        }
-      </Routes>
+      <home>
+        <Routes>
+          {<Route path="/" element={<Home />}></Route>}
+          {<Route path="/:id" element={<Main />} />}
+          {
+            <Route
+              path="/:SpecificProfile/:id"
+              element={<SpecificProfileViewer />}
+            />
+          }
+        </Routes>
       </home>
     </BrowserRouter>
   );
