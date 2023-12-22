782b848dcabb1da9d1c49cb9f4799108e526f7dc
SERENABACCARI <serena88_2011@libero.it>
2023-12-22 15:09:57 +0100

    home

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
 import Main from "./components/Main";
 import SpecificProfileViewer from "./components/SpecificProfileViewer";
+import Home from "./components/Home.js"
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -9,14 +10,16 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Home>
         {<Route path="/" element={<Main />}></Route>}
         {<Route path="/:id" element={<SpecificProfileViewer />} />}
         {
           <Route
-            path="/:SpecificProfile/:id"
-            element={<SpecificProfileViewer />}
+          path="/:SpecificProfile/:id"
+          element={<SpecificProfileViewer />}
           />
         }
+      </Home>
       </Routes>
     </BrowserRouter>
   );
